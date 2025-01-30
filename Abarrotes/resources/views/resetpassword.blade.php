04b91887f2fd999c0378c0f4d7360b1478ac02ca
Brandon Eduardo <torresrojasb013@gmail.com>
2025-01-30 16:23:29 -0600

    Anexo de login
    Se agrega el formulario de inicio de sesion

--- Abarrotes/resources/views/resetpassword.blade.php
+++ Abarrotes/resources/views/resetpassword.blade.php
@@ -1,16 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/estilo.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-    <title>Login</title>
-</head>
-<body>  
 <div class="wrapper fadeInDown">
       <div id="formContent">
         
@@ -25,6 +12,4 @@
         </form>
     
       </div>
-    </div>
-</body>
-</html>
+</div>
