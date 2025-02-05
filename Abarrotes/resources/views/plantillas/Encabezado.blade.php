4ff54652219a74d34c5c6df7416fbb931c1dcf92
Luis Antonio Zermeño Muñoz <xrubiomargodx@gmail.com>
2025-02-05 15:12:40 -0600

    index terminado
    ya quedo el encabezado

--- Abarrotes/resources/views/plantillas/Encabezado.blade.php
+++ Abarrotes/resources/views/plantillas/Encabezado.blade.php
@@ -1,15 +1,18 @@
 <link rel="stylesheet" href="{{asset('css/styleLuis.css')}}">
 <header>
-  <div class="CONTENEDOR">
-    <figure>
-      <div class="CONTENEDOR">
-        <img src="#" alt="logo" />
-      </div>
+  <div class="CONTENEDOR-HEADER">
+    <figure class="CONTENEDOR-LOGO">
+      <img src="{{asset('img/LOGO.png')}}" alt="logo" />
     </figure>
     <nav>
-      <div class="CONTENEDOR">
-        <a href="{{route('Clientes')}}">Clientes</a>
-        <a href="{{route('Pedidos')}}">Pedidos</a>
+      <div class="CONTENEDOR-NAV">
+        <a href="{{route('Clientes')}}">
+          <img src="{{asset('img/CLIENTES.ico')}}">Productos</a>
+        <a href="{{route('Pedidos')}}">
+          <img src="{{asset('img/PEDIDOS.ico')}}">Pedidos</a>
+        <a href="{{route('login')}}">
+          <img src="{{asset('img/SALIR.ico')}}">Salir
+        </a>
       </div>
     </nav>
   </div>
--- Abarrotes/routes/web.php
+++ Abarrotes/routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
   return view('login');
-});
+})->name('login');
 
 Route::get('/Inicio', function () {
   return view('app');
